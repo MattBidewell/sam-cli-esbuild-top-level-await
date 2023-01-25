e13b405a6a6435dc3ce24870b983268df342b9db
Matthew Bidewell <matthewbidewell@gmail.com>
2023-01-25 12:29:33 +0000

    Updated problem

--- src/handler.ts
+++ src/handler.ts
@@ -4,12 +4,18 @@ export class Lambda {
   public handler() {
     return {
       statusCode: 200,
-      body: "hello world",
+      body: "hello world2.0123456",
     }
   }
 }
 
-const promise = Promise.resolve(() => {console.log("test")});
+const promise = new Promise<void>((resolve) => {
+  setTimeout(() => {
+    console.log("timeout!")
+    resolve()
+  }, 1000)
+});
+
 await promise;
 
 const handlerClass = new Lambda();
